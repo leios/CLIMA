bdd3d77849c21adefb01a43d33de1a7e67625083
christophernhill <cnh@mit.edu>
2020-12-02 22:12:53 -0500

    More

---
+++ test/Ocean/OcnCadj/OCNCADJEEquationSet.jl
@@ -0,0 +1,14 @@
+"""
+    module OCNCADJEEquationSet
+   
+ Defines an equation set for an explicit convective adjustment scheme for ocean.
+
+ Defines kernels to evaluates RHS of
+
+  d phi / dt = - d/dz k d/dz phi
+
+ subject to Neumann (prescribed gradient) bc's
+
+"""
+module OCNCADJEEquatioonSet
+end
